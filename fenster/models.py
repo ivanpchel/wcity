e0f87acf13a0042e4ce75b97b31cfc3ac26d735c
ivanpchel <ivanpchel@yandex.ru>
2019-04-18 21:07:53 +0300

    Список окон, количество продано/на складе

--- fenster/models.py
+++ fenster/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import int_list_validator
 
 # Create your models here.
 class Fenster (models.Model):
@@ -6,3 +7,9 @@ class Fenster (models.Model):
     window_height = models.IntegerField()
     window_number = models.IntegerField(default=0)
     window_view = models.CharField(default='', max_length=1024)
+    window_scheme = models.CharField(
+	validators=[int_list_validator],
+	default='1,2',
+	max_length=1024
+    )
+    is_buy = models.BooleanField(default=False)
--- fenster/views.py
+++ fenster/views.py
@@ -15,15 +15,30 @@ def index(request):
    # test_creation(3)
    # test_creation(4)
    # test_creation(5)
-    
-    fenster_list = Fenster.objects.order_by("id")
-    
-    context = {
-	"window_list": fenster_list,	
-    }
 
-    #return HttpResponse(template.render(context))
-    return render(request, 'fenster/index.html', context)
+    try:
+
+        if request.method == 'POST':
+            fenster_id = request.POST['selected_fenster']
+            buy(fenster_id)
+        
+        fenster_list = Fenster.objects.filter(is_buy=False).order_by("id")
+        buyed_count = Fenster.objects.filter(is_buy=True).count()
+        in_stock_count = len(fenster_list)   	
+        context = {
+            "window_list": fenster_list,
+            "buyed_count": buyed_count,
+            "in_stock_count": in_stock_count,	
+         }
+
+        return render(request, 'fenster/index.html', context)
+    except:
+        i = 1
+	
+def buy(fenster_id):
+    Fenster.objects.filter(id=fenster_id).update(is_buy=True)
+    #правильней получать через get
+    #Fenster.objects.filter(id=fenster_id).delete()
 
 def test_creation(number):
 
